78d87123bfe14cd5f0545858dd00bb85fdc15ae5
Pasindu Yapa <lolmixage03@gmail.com>
2025-09-21 09:55:30 +0530

    Feature: Nes.css fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {Press_Start_2P} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const pressStart2P = Press_Start_2P({
+    weight: "400",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={pressStart2P.className}
       >
         {children}
       </body>
